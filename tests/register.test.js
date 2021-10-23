a6ce5ff3318a1073cf43c1ffb98ebd24d5332807
YoYolops <root@DESKTOP-D8D0H6O.localdomain>
2021-10-23 17:46:44 -0300

    a

--- tests/register.test.js
+++ tests/register.test.js
@@ -2,7 +2,7 @@ import app from '../src/index';
 import supertest from 'supertest';
 import connection from '../src/database/database';
 
-describe("POST /register", () => {
+describe("POST /register", async () => {
     beforeAll(async () => {
         await connection.query('DELETE FROM users;')
         await connection.query('DELETE FROM sessions;')
