635cf56da13a74633bc1364eadd849cbb0961f2d
YoYolops <root@DESKTOP-D8D0H6O.localdomain>
2021-10-23 18:02:24 -0300

    d

--- tests/register.test.js
+++ tests/register.test.js
@@ -2,7 +2,7 @@ import app from '../src/index';
 import supertest from 'supertest';
 import connection from '../src/database/database';
 
-describe("POST /register", () => {
+describe('POST /register', () => {
     beforeAll(async () => {
         await connection.query('DELETE FROM users;')
         await connection.query('DELETE FROM sessions;')
@@ -11,63 +11,63 @@ describe("POST /register", () => {
 
     afterAll(() => { connection.end() })
 
-    it("register POST /register without username returns 422", async () => {
+    it('register POST /register without username returns 422', async () => {
         const body = {
-            name: "",
-            email: "yoyo2@gmail.com",
-            password: "yoyo"
+            name: '',
+            email: 'yoyo2@gmail.com',
+            password: 'yoyo'
         }
 
-        const result = await supertest(app).post("/register").send(body);
+        const result = await supertest(app).post('/register').send(body);
         const status = result.statusCode
         expect(status).toEqual(422)
     })
 
-    it("register POST /register without password returns 422", async () => {
+    it('register POST /register without password returns 422', async () => {
         const body = {
-            name: "yoyo",
-            email: "yoyo2@gmail.com",
-            password: ""
+            name: 'yoyo',
+            email: 'yoyo2@gmail.com',
+            password: ''
         }
 
-        const result = await supertest(app).post("/register").send(body);
+        const result = await supertest(app).post('/register').send(body);
         const status = result.statusCode
         expect(status).toEqual(422)
     })
 
-    it("register POST /register without email returns 422", async () => {
+    it('register POST /register without email returns 422', async () => {
         const body = {
-            name: "yoyo",
-            email: "",
-            password: "yoyo"
+            name: 'yoyo',
+            email: '',
+            password: 'yoyo'
         }
 
-        const result = await supertest(app).post("/register").send(body);
+        const result = await supertest(app).post('/register').send(body);
         const status = result.statusCode
         expect(status).toEqual(422)
     })
 
-    it("register POST /register already registered user", async () => {
+    it('register POST /register already registered user', async () => {
         const body = {
-            name: "yoyo",
-            email: "yoyo@gmail.com",
-            password: "yoyo"
+            name: 'yoyo',
+            email: 'yoyo@gmail.com',
+            password: 'yoyo'
         }
 
-        const result = await supertest(app).post("/register").send(body);
+        const result = await supertest(app).post('/register').send(body);
         const status = result.statusCode
         expect(status).toEqual(409)
     })
 
-    it("register POST /register a proper user", async () => {
+    it('register POST /register a proper user', async () => {
         const body = {
-            name: "yoyo",
-            email: "yoyo3@gmail.com",
-            password: "yoyo"
+            name: 'yoyo',
+            email: 'yoyo3@gmail.com',
+            password: 'yoyo'
         }
 
         const result = await supertest(app)
-            .post("/register")
+            .post('/register')
             .send(body)
             .expect(res => {
                 console.log(res.body)
