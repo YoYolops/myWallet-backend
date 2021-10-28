4a5fdb6825a93445d7bae4885fdc5bcd9fb4a668
Ligia D'Avila Bozzi <ligia@respondeai.com.br>
2021-10-28 09:08:11 -0300

    preparando review

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server running on port ${process.env.SERVER_PORT}`)
+app.listen(4000, () => {
+    console.log(`Server running on port 4000`)
 })
