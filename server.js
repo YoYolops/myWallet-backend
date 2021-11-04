a2520aeb2e693c26f4dfb48cfba2791cc4508836
Yohan Lopes <yohanhxh@gmail.com>
2021-11-04 11:53:59 -0300

    deploy/ preparing env variables

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log("Server running on port " + process.env.SERVER_PORT);
-});
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
